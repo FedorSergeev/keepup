c2e33be247d0e04c5ba127318c47b98a0259426a
fedor.sergeev <f.sergeev@trans-it.pro>
2021-10-31 19:19:26 +0300

    Nebular dashboard inside as KeepUP plugin

---
+++ nebular-ui/src/main/java/io/keepup/plugins/NebularUiService.java
@@ -0,0 +1,45 @@
+package io.keepup.plugins;
+
+import io.keepup.cms.core.annotation.Deploy;
+import io.keepup.cms.core.annotation.Plugin;
+import io.keepup.cms.core.commons.ApplicationConfig;
+import io.keepup.cms.core.plugins.AbstractKeepupDeployBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+/**
+ * KeepUP plugin service providing special UI for administrative purposes
+ */
+@Plugin
+@Deploy
+@Service
+@ConditionalOnProperty(prefix = "keepup.plugins.nebularui", name = "enabled", havingValue = "true")
+public class NebularUiService extends AbstractKeepupDeployBean {
+
+    @Value("${keepup.plugins.nebularui.uri:/**}")
+    private String dashboardUri;
+    @Value("${keepup.plugins.nebularui.enabled:false}")
+    private boolean isEnabled;
+
+    @Autowired
+    private ApplicationConfig applicationConfig;
+
+    @Bean
+    public RouterFunction<ServerResponse> imgRouter() {
+        logger.debug("Setting URI for Nebular UI dashboard with value = %s".formatted(dashboardUri));
+        return RouterFunctions.resources(dashboardUri,
+                new FileSystemResource("%s/resources".formatted(applicationConfig.getDocumentRoot())));
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
