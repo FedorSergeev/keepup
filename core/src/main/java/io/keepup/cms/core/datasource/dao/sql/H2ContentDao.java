419689ee9f9b982b6f66626a026017221fe0f8d8
fedor.sergeev <f.sergeev@trans-it.pro>
2022-02-06 01:50:11 +0300

    #52 Implement method for getting the child node parents for H2 data source
    As H2 does not support PostgreSQL recursive queries, we need to implement functionality for emitting the specified content record parents for this type of data source.
    
    H2 can be used as a master db in case when you want to run an application (doesn't matter containerized or not) as the only component for demonstration purposes.
    
    Records that are being emitted aren't stored in Content cache.
    
    There is a couple of small SQL code optimizations.

---
+++ core/src/main/java/io/keepup/cms/core/datasource/dao/sql/H2ContentDao.java
@@ -0,0 +1,60 @@
+package io.keepup.cms.core.datasource.dao.sql;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.keepup.cms.core.cache.CacheAdapter;
+import io.keepup.cms.core.datasource.sql.repository.ReactiveNodeAttributeEntityRepository;
+import io.keepup.cms.core.datasource.sql.repository.ReactiveNodeEntityRepository;
+import io.keepup.cms.core.persistence.Content;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+/**
+ * Service for working with {@link io.keepup.cms.core.persistence.Content} records using H2 database
+ * as data source. Can be used for demonstration properties, e.g. if you want to run the app without
+ * any additional components like production database.
+ *
+ * Service works in 'h2' profile.
+ *
+ * @author Fedor Sergeev
+ * @since  2.0.0
+ */
+@Service
+@Primary
+@Profile("h2")
+public class H2ContentDao extends SqlContentDao {
+    public H2ContentDao(ReactiveNodeEntityRepository reactiveNodeEntityRepository, ReactiveNodeAttributeEntityRepository reactiveNodeAttributeEntityRepository, ObjectMapper objectMapper, CacheManager manager, CacheAdapter adapter) {
+        super(reactiveNodeEntityRepository, reactiveNodeAttributeEntityRepository, objectMapper, manager, adapter);
+    }
+
+    /**
+     * Less effective method to fetch record parents, but it works for H2 database. Fetches all {@link Content} records
+     * until the root or the specified offset record is found. Yet this method does not put found records in cache.
+     *
+     * @param id     first parent record identifier, in case of null empty Flux will be returned
+     * @param offset depth of search, in case of null will be set to {@link Long#MAX_VALUE}
+     * @return       publisher for the sequence of records inheriting each other till the record with the specified
+     *               parent id (excluding this record itself)
+     */
+    @Override
+    public Flux<Content> getContentParents(@NotNull Long id, @Nullable Long offset) {
+        // left this check for public API users who don't use any lint tools
+        if (id == null) {
+            getLog().error("Null parameter id was passed to getContentParents method");
+            return Flux.empty();
+        }
+        int capacity = offset == null || offset > Integer.MAX_VALUE
+            ? Integer.MAX_VALUE
+            : offset.intValue();
+
+        return getNodeEntityRepository().findById(id)
+                .flatMap(entity -> getNodeEntityRepository().findById(entity.getParentId()))
+                .expand(nodeEntity -> getNodeEntityRepository().findById(nodeEntity.getParentId()), capacity)
+                .take(capacity)
+                .flatMap(getNodeEntityPublisherFunction());
+    }
+}
---
+++ core/src/test/java/io/keepup/cms/core/datasource/dao/sql/H2ContentDaoTest.java
@@ -0,0 +1,141 @@
+package io.keepup.cms.core.datasource.dao.sql;
+
+import io.keepup.cms.core.boot.KeepupApplication;
+import io.keepup.cms.core.cache.CacheAdapter;
+import io.keepup.cms.core.cache.KeepupCacheConfiguration;
+import io.keepup.cms.core.config.DataSourceConfiguration;
+import io.keepup.cms.core.config.R2dbcConfiguration;
+import io.keepup.cms.core.config.WebFluxConfig;
+import io.keepup.cms.core.datasource.sql.H2ConsoleService;
+import io.keepup.cms.core.datasource.sql.repository.ReactiveNodeAttributeEntityRepository;
+import io.keepup.cms.core.datasource.sql.repository.ReactiveNodeEntityRepository;
+import io.keepup.cms.core.datasource.sql.repository.ReactiveUserEntityRepository;
+import io.keepup.cms.core.persistence.Content;
+import io.keepup.cms.core.persistence.Node;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles({"dev", "h2"})
+@ContextConfiguration(classes = {
+        KeepupApplication.class,
+        KeepupCacheConfiguration.class,
+        CacheAdapter.class,
+        WebFluxConfig.class,
+        ReactiveNodeEntityRepository.class,
+        ReactiveNodeAttributeEntityRepository.class,
+        ReactiveUserEntityRepository.class,
+        DataSourceConfiguration.class,
+        H2ConsoleService.class,
+        H2ConsoleService.class,
+        R2dbcConfiguration.class,
+        H2ContentDao.class,
+})
+@DataR2dbcTest
+class H2ContentDaoTest {
+
+    @Autowired
+    H2ContentDao h2ContentDao;
+
+    @Test
+    void getContentParents() {
+        h2ContentDao.getNodeAttributeEntityRepository().deleteAll()
+                .then(h2ContentDao.getNodeEntityRepository().deleteAll())
+                .then(h2ContentDao.createContent(createRecord(0L, Pair.of("key", "firstParent"))))
+                .flatMap(grandParent -> h2ContentDao.createContent(createRecord(grandParent, Pair.of("key", "secondParent"))))
+                .flatMap(parent -> h2ContentDao.createContent(createRecord(parent, Pair.of("key", "child"))))
+                .flatMap(childId -> h2ContentDao.getContentParents(childId, 2L).collectList())
+                .doOnNext(parents -> {
+                    assertNotNull(parents);
+                    assertFalse(parents.isEmpty());
+                    assertEquals(2, parents.size());
+                    assertEquals(parents.get(0).getParentId(), parents.get(1).getId());
+                    assertEquals(0, parents.get(1).getParentId());
+                })
+                .block();
+    }
+
+    @Test
+    void getContentParentsByNullId() {
+        h2ContentDao.getNodeAttributeEntityRepository().deleteAll()
+                .then(h2ContentDao.getNodeEntityRepository().deleteAll())
+                .then(h2ContentDao.createContent(createRecord(0L, Pair.of("key", "firstParent"))))
+                .flatMap(grandParent -> h2ContentDao.createContent(createRecord(grandParent, Pair.of("key", "secondParent"))))
+                .flatMap(parent -> h2ContentDao.createContent(createRecord(parent, Pair.of("key", "child"))))
+                .then(h2ContentDao.getContentParents(null, 2L).collectList())
+                .doOnNext(parents -> {
+                    assertNotNull(parents);
+                    assertTrue(parents.isEmpty());
+                })
+                .block();
+    }
+
+    @Test
+    void getContentParentsByNullOffset() {
+        h2ContentDao.getNodeAttributeEntityRepository().deleteAll()
+                .then(h2ContentDao.getNodeEntityRepository().deleteAll())
+                .then(h2ContentDao.createContent(createRecord(0L, Pair.of("key", "firstParent"))))
+                .flatMap(grandParent -> h2ContentDao.createContent(createRecord(grandParent, Pair.of("key", "secondParent"))))
+                .flatMap(parent -> h2ContentDao.createContent(createRecord(parent, Pair.of("key", "child"))))
+                .flatMap(childId -> h2ContentDao.getContentParents(childId, null).collectList())
+                .doOnNext(parents -> {
+                    assertNotNull(parents);
+                    assertFalse(parents.isEmpty());
+                    assertEquals(2, parents.size());
+                    assertEquals(parents.get(0).getParentId(), parents.get(1).getId());
+                    assertEquals(0, parents.get(1).getParentId());
+                })
+                .block();
+    }
+
+    @Test
+    void getContentParentsByVeryLongOffset() {
+        h2ContentDao.getNodeAttributeEntityRepository().deleteAll()
+                .then(h2ContentDao.getNodeEntityRepository().deleteAll())
+                .then(h2ContentDao.createContent(createRecord(0L, Pair.of("key", "firstParent"))))
+                .flatMap(grandParent -> h2ContentDao.createContent(createRecord(grandParent, Pair.of("key", "secondParent"))))
+                .flatMap(parent -> h2ContentDao.createContent(createRecord(parent, Pair.of("key", "child"))))
+                .flatMap(childId -> h2ContentDao.getContentParents(childId, Long.MAX_VALUE).collectList())
+                .doOnNext(parents -> {
+                    assertNotNull(parents);
+                    assertFalse(parents.isEmpty());
+                    assertEquals(2, parents.size());
+                    assertEquals(parents.get(0).getParentId(), parents.get(1).getId());
+                    assertEquals(0, parents.get(1).getParentId());
+                })
+                .block();
+    }
+
+    @Test
+    void getContentParentsByOffsetIsOne() {
+        h2ContentDao.getNodeAttributeEntityRepository().deleteAll()
+                .then(h2ContentDao.getNodeEntityRepository().deleteAll())
+                .then(h2ContentDao.createContent(createRecord(0L, Pair.of("key", "firstParent"))))
+                .flatMap(grandParent -> h2ContentDao.createContent(createRecord(grandParent, Pair.of("key", "secondParent"))))
+                .flatMap(parent -> h2ContentDao.createContent(createRecord(parent, Pair.of("key", "child"))))
+                .flatMap(childId -> h2ContentDao.getContentParents(childId, 1L).collectList())
+                .doOnNext(parents -> {
+                    assertNotNull(parents);
+                    assertFalse(parents.isEmpty());
+                    assertEquals(1, parents.size());
+                })
+                .block();
+    }
+
+    Content createRecord(long parentId, Pair<String, String> attribute) {
+        Content node = new Node();
+        node.setDefaultPrivileges();
+        node.setParentId(parentId);
+        node.setOwnerId(0L);
+        node.setAttribute(attribute.getLeft(), attribute.getRight());
+        return  node;
+    }
+}
